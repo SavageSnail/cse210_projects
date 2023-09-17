e9f2f2716f9d2d6ac1f8238d04b57d35d596a6ab
SavageSnail <peyton.bledsoe@gmail.com>
2023-09-16 23:51:16 -0600

    Saving the first two steps

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string valueFromUser = Console.ReadLine();
+
+        int x = int.Parse(valueFromUser);
+
+        if (x >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (x >=80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (x >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (x >=60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
+
+        if (x >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+
+        else 
+        {
+            Console.WriteLine("Oof. Try again next semester!");
+        }
+
     }
 }
