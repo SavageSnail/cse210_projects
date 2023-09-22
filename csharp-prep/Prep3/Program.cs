3316da5d167a2d2d338e2452ac7f7ea9fff2da77
SavageSnail <peyton.bledsoe@gmail.com>
2023-09-21 23:57:58 -0600

    Finished the random guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       
+        int guess = -1;
+
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 101);
+        
+        
+    while (guess != magic)
+    {
+
+        Console.Write("What is your guess? ");
+        string userGuess = Console.ReadLine();
+
+        guess = int.Parse(userGuess);
+
+
+        if (guess > magic)
+        {
+            Console.WriteLine("Lower");
+          
+        }
+
+        else if (guess < magic)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        else
+        {
+            Console.WriteLine("You guessed Right!");
+        }
     }
+
+
+
+   
+
+
+     }
 }
